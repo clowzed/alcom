2ae00c0113d1ffe0a91e6c1662ac9f1550c138c1
YoungMeatBoy <d2ms2nk@gmail.com>
2020-07-17 13:52:31 +0300

    init added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 		name             = 'alcom',
-		version          = '1.0.0',
+		version          = '1.0.1',
 		packages         = find_packages(),
 		author           = 'Miasnenko Dmitry',
 		author_email     = 'cl0wzed.exe@gmail.com',
